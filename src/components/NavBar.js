66a7f936b149853815bef7514b80c88287154f5a
Roger Perez <roger.perezcol@gmail.com>
2022-04-25 20:52:56 -0300

    menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import logo from '../images/vw.png'
+
+
+const Nav = () => {
+  return (
+  <nav>
+ 
+<ul className="menu__bar"> 
+  <img src={logo} alt="vw" width="65vh" height = "65vh" />
+  <li className='menu__bar-link'>Volkswagen</li>
+  <li className='menu__bar-link'>Modelos</li>
+  <li className='menu__bar-link'>Usados</li>
+  <li className='menu__bar-link'>Financiacion</li>
+  
+  </ul>
+</nav>
+  )
+}
+
+export default Nav
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Comentando el inicio de react
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Se debe tener la variable react en scope
+import React from "react"
+// Se debe tener la variable reactDom en scope
+import  ReactDOM  from "react-dom/client"
+import Nav from "./components/NavBar"
+import "./style.scss"
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-//
+
+// Tener la variale App en Scope la var app es toda la aplicacion en la que vamos a trabajar
+const App = () => {
+    return <Nav/>
+}
+
+// Se renderiza para poder mostrar la funcion previamente creada como app()o <App/> en jsx,Destino donde se muestra en el dom)
+//Forma vija  ReactDOM.render(<App/>,document.getElementById ("root") )
+root.render(<App/>)
